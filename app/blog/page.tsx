8c425fddb1aab880ff0ad31b63bbcbc10b4a6323
Edogawa Tr <nghiatran@Edogawas-MacBook-Air.local>
2025-11-12 22:03:24 +0700

    Initial commit: setup Next.js + Bun + PayloadCMS

---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { DivideCircle } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Button variant="destructive">
+      Hello world
+    </Button>
+  );
+}
