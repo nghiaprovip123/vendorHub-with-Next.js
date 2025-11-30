ac6b1dd4b72776c3fddc0df74bfd3e02cbade83d
nghiaprovip123 <wrefree549@gmail.com>
2025-11-30 13:14:16 +0700

    api get products

---
+++ app/api/products/route.ts
@@ -0,0 +1,48 @@
+import { prisma } from "@/lib/prisma"; // Corrected import
+import { NextResponse } from "next/server";
+
+// GET PRODUCT ---> NEED TO DOCUMENT ON CONFLUENCE
+export async function GET(req: Request) {
+    try {
+        const products = await prisma.product.findMany()
+        return NextResponse.json(
+            {message: "fetch successfully products", products: products},
+            {status: 201}
+        )
+    }
+    catch (err:any) {
+        return NextResponse.json (
+            {error: "fail to fetch products"},
+            {status: 401}
+        )
+    }
+}
+// export async function POST(req:Request) {
+//     const newProduct = await prisma.product.create({
+//         data: {
+//             pid: "p001",
+//             title: "Test Product",
+//             price: 99,        
+//         }
+//     })
+//     return NextResponse.json(
+//             { message: "product is create successfully", product: newProduct },
+//             { status: 500 }
+//     )
+// }
+
+
+// CREATE PRODUCT ---> NEED TO DOCUMENT ON CONFLUENCE
+export async function POST(req:Request) {
+    const newProduct = await prisma.product.create({
+        data: {
+            pid: "p001",
+            title: "Test Product",
+            price: 99,        
+        }
+    })
+    return NextResponse.json(
+            { message: "product is create successfully", product: newProduct },
+            { status: 500 }
+    )
+}
