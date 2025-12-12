6e84e886c6d16f707ab70c3b2834fc8ee989d96b
nghiaprovip123 <wrefree549@gmail.com>
2025-12-12 10:35:09 +0700

    Using JWT-based Authentication with token refresh

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -2,6 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 import bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 import jwt from "jsonwebtoken";
+import { getSession } from "@/lib/session-helper"
 
 export async function POST(req: NextRequest) {
     try {
@@ -27,13 +28,36 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    const token = jwt.sign(
+    const accessToken = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: "15m" }
+    );
+
+    const refreshToken = jwt.sign(
       { id: user.id, email: user.email },
       process.env.JWT_SECRET!,
       { expiresIn: "7d" }
     );
+    
+    const response = NextResponse.json({ 
+      message: "Login successful",
+      accessToken 
+    });
 
-    return NextResponse.json({ token });
+    response.cookies.set("refresh_token", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    });
+
+    return response;
+
+    return NextResponse.json(
+      { message: "Login successfully", accessToken }
+    );
   } catch (err) {
     return NextResponse.json(
       { error: "Unknown Error" },
